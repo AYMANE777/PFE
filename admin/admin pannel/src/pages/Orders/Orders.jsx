5fd67a92d2962d02835bb358e8880f88e722c2c1
RAYMANE <briouelaymane0@gmail.com>
2025-06-06 13:49:04 +0100

    Update admin pannel

--- admin/admin pannel/src/pages/Orders/Orders.jsx
+++ admin/admin pannel/src/pages/Orders/Orders.jsx
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Orders.css"
-function Orders(props) {
+import axios from "axios";
+import {toast} from "react-toastify";
+import {assets} from "../../assets/assets.js";
+function Orders({url}) {
+    const [orders, setOrders] = useState([])
+    const fetchAllOrders = async () => {
+        const res = await axios.get(url+"/api/order/list")
+        if(res.data.success){
+            setOrders(res.data.data)
+            console.log(res.data.data)
+        }
+        else{
+            toast.error("Error getting orders")
+        }
+    }
+const statusHandler = async (event,orderId) => {
+        try {
+            const res = await axios.get(url+"/api/order/status",{
+                orderId,
+                status:event.target.value
+            })
+            if(res.data.success){
+                await fetchAllOrders()
+            }else{
+
+            }
+        }catch (error) {
+
+        }
+
+}
+    useEffect(()=>{
+        fetchAllOrders()
+    },[])
     return (
-        <div></div>
+        <div className="order add">
+            <h3>Order Page</h3>
+            <div className="order-list">
+                {orders.map((order, index) => (
+                    <div key={index} className="order-item">
+                        <img src={assets.parcel_icon} alt=""/>
+                        <div>
+                            <p className="order-item-food">
+                                {order.items.map((item, index) => {
+                                    if (index === order.items.length - 1) {
+                                        return item.name + " x " + item.quantity;
+
+                                    } else {
+                                        return item.name + " x " + item.quantity + ", ";
+                                    }
+                                })}
+                            </p>
+                            <p className="order-item-name">{order.address.firstName + " " + order.address.lastName}</p>
+                            <div className="order-item-address">
+                                <p>{order.address.street + ","}</p>
+                                <p>{order.address.city + "," + order.address.state + ", " + order.address.country+", "+order.address.zipcode}</p>
+                            </div>
+                            <p className="order-item-phone">{order.address.phone}</p>
+                        </div>
+                        <p>Items : {order.items.length}</p>
+                        <p>${order.items.amount}</p>
+                        <select onChange={(e)=>statusHandler(e,order._id)} value={order.status}>
+                            <option value="Food Processing">Food Processing</option>
+                            <option value="Out for delivery">Out for delivery</option>
+                            <option value="Delivered">Delivered</option>
+                        </select>
+                    </div>
+                ))}
+            </div>
+</div>
     );
 }
 
